32fcae4f0480a9dc366cb82cccacc060a83ad287
Sahil Gautam <sahil@libreoffice.org>
2024-08-09 01:32:18 +0530

    build suckless

--- 04-files.sh
+++ 04-files.sh
@@ -11,3 +11,8 @@ cd $HOME/.dotfiles/.local/src
 sudo cp 00-keyboard.conf    /etc/X11/xorg.conf.d/00-keyboard.conf
 sudo cp 30-touchpad.conf    /etc/X11/xorg.conf.d/30-touchpad.conf
 sudo cp dwm.desktop         /usr/share/xsessions/dwm.desktop
+
+sudo cp /usr/share/fontconfig/conf.avail/75-twemoji.conf /etc/fonts/conf.d/75-twemoji.conf
+
+# build suckless
+sh $HOME/.dotfiles/.local/bin/buildsuckless
--- 05-system.sh
+++ 05-system.sh
@@ -4,3 +4,5 @@
 # change the system shell
 chsh -s $(which zsh)
 sudo chsh -s $(which zsh) root
+
+reboot
